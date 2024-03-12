03ab97e998e1d17a44d74fcffaf2de4190935a50
Lucas Morais <lucas.axm@gmail.com>
2024-03-12 17:18:49 -0300

    First commit

---
+++ src/main/java/com/boatarde/bilubot/BilubotApplication.java
@@ -0,0 +1,13 @@
+package com.boatarde.bilubot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilubotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilubotApplication.class, args);
+    }
+
+}
